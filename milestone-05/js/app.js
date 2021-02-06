de11aca4f8e1c2e50a93cd768232cea5aca64a5b
alamgir8 <ahossain38900@gmail.com>
2021-02-07 04:10:18 +0600

    half-complete

--- milestone-05/js/app.js
+++ milestone-05/js/app.js
@@ -1,6 +1,10 @@
 
 function getRequireFood() {
-   const inputFoodName = document.getElementById('input-food').value;
+let inputFoodName = document.getElementById('input-food').value;
+if (inputFoodName == '') {
+    alert('please inter valid food name');
+    return;
+}
 fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f&s=${inputFoodName}`)
 .then(response => response.json())
 .then(data => displayFood(data));
@@ -16,17 +20,42 @@ const displayFood = food =>{
 
         foodItemDiv.className = 'food-name'
         const foodDescription = `
-        <a onclick="displayFoodIngredient('foodId')"> <img src=${food.strMealThumb}>
-        <h2>${food.strMeal}</h2></a>
+        <span  onclick="displayFoodIngredient('${food.strMeal}')">
+        <div id="food-details"> </div>        
+        <img src=${food.strMealThumb}>
+        <h2>${food.strMeal}</h2></span>
         `
         foodItemDiv.innerHTML = foodDescription;
         foodList.appendChild(foodItemDiv);
     })
 }
 
-const displayFoodIngredient = foodId =>{
-    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${foodId}`)
+const displayFoodIngredient = foodName =>{
+    
+    
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?f&s=${foodName}`
+    fetch(url)
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => foodDetails(data.meals[0]))
+  
+
 }
-displayFoodIngredient();
+
+const foodDetails = food =>{
+    const foodDetails = document.getElementById('food-details');
+
+    foodDetails.innerHTML = `
+    <img src=${food.strMealThumb}>
+    <h2>${food.strMeal}</h2>
+    <h4>Ingredient:</h4><br>
+    ${food.strMeasure1} ${food.strIngredient1}
+    ${food.strMeasure2} ${food.strIngredient2}
+    ${food.strMeasure3} ${food.strIngredient3}
+    ${food.strMeasure4} ${food.strIngredient4}
+    ${food.strMeasure5} ${food.strIngredient5}
+    ${food.strMeasure6} ${food.strIngredient6}
+    ${food.strMeasure7} ${food.strIngredient7}
+    ${food.strMeasure8} ${food.strIngredient8}
+    ${food.strMeasure9} ${food.strIngredient9}
+    `
+}
