4db0e1cc3f8ba95414ca86bc5773a02e6f83b25f
alamgir8 <ahossain38900@gmail.com>
2021-02-07 06:36:08 +0600

    quarter complete

--- milestone-05/js/app.js
+++ milestone-05/js/app.js
@@ -1,61 +1,72 @@
 
-function getRequireFood() {
-let inputFoodName = document.getElementById('input-food').value;
-if (inputFoodName == '') {
-    alert('please inter valid food name');
-    return;
-}
-fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f&s=${inputFoodName}`)
-.then(response => response.json())
-.then(data => displayFood(data));
+    function getRequireFood() {
+        let inputFoodName = document.getElementById('input-food').value;
+        if (inputFoodName == '') {
+            alert('please inter valid recipe items name');
+            return;
+        }
+        if (inputFoodName == 1) {
+            document.getElementById('food-list').style.display = inputFoodName;
+        }
+        fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f&s=${inputFoodName}`)
+        .then(response => response.json())
+        .then(data => displayFood(data));
 
-}
+        document.getElementById('input-food').value = '';
 
 
-const displayFood = food =>{
-    console.log(food);
-    const foodList = document.getElementById('food-list');
-    food.meals.forEach(food => {
-        const foodItemDiv = document.createElement('div');
+        }
+      
 
-        foodItemDiv.className = 'food-name'
-        const foodDescription = `
-        <span  onclick="displayFoodIngredient('${food.strMeal}')">
-        <div id="food-details"> </div>        
+/*==========================function for display search recipe=======================*/
+    const displayFood = food =>{
+    
+        const foodList = document.getElementById('food-list');
+        food.meals.forEach(food => {
+            const foodItemDiv = document.createElement('div');
+
+            foodItemDiv.className = 'food-name'
+            const foodDescription = `
+            <span  onclick="displayFoodIngredient('${food.strMeal}')">
+                
+            <img src=${food.strMealThumb}>
+            <h2>${food.strMeal}</h2></span>
+            `
+            foodItemDiv.innerHTML = foodDescription;
+            foodList.appendChild(foodItemDiv);
+        })
+    }
+
+/*==========================function for display recipe ingredient=======================*/
+    const displayFoodIngredient = foodName =>{
+
+        fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f&s=${foodName}`)
+        .then(res => res.json())
+        .then(data => foodDetails(data.meals[0]))
+    
+
+    }
+
+
+/*==========================function for display recipe details information=======================*/    
+    const foodDetails = food =>{
+        const foodImage = document.getElementById('food-image');
+        const foodDetails = document.getElementById('food-details');
+        
+        foodImage.innerHTML = `
         <img src=${food.strMealThumb}>
-        <h2>${food.strMeal}</h2></span>
         `
-        foodItemDiv.innerHTML = foodDescription;
-        foodList.appendChild(foodItemDiv);
-    })
-}
-
-const displayFoodIngredient = foodName =>{
-    
-    
-    const url = `https://www.themealdb.com/api/json/v1/1/search.php?f&s=${foodName}`
-    fetch(url)
-    .then(res => res.json())
-    .then(data => foodDetails(data.meals[0]))
-  
-
-}
-
-const foodDetails = food =>{
-    const foodDetails = document.getElementById('food-details');
-
-    foodDetails.innerHTML = `
-    <img src=${food.strMealThumb}>
-    <h2>${food.strMeal}</h2>
-    <h4>Ingredient:</h4><br>
-    ${food.strMeasure1} ${food.strIngredient1}
-    ${food.strMeasure2} ${food.strIngredient2}
-    ${food.strMeasure3} ${food.strIngredient3}
-    ${food.strMeasure4} ${food.strIngredient4}
-    ${food.strMeasure5} ${food.strIngredient5}
-    ${food.strMeasure6} ${food.strIngredient6}
-    ${food.strMeasure7} ${food.strIngredient7}
-    ${food.strMeasure8} ${food.strIngredient8}
-    ${food.strMeasure9} ${food.strIngredient9}
-    `
-}
+        foodDetails.innerHTML = `
+        <h1>${food.strMeal}</h1><br>
+        <h4>Ingredients:</h4>
+        ${food.strMeasure1} ${food.strIngredient1}<br>
+        ${food.strMeasure2} ${food.strIngredient2}<br>
+        ${food.strMeasure3} ${food.strIngredient3}<br>
+        ${food.strMeasure4} ${food.strIngredient4}<br>
+        ${food.strMeasure5} ${food.strIngredient5}<br>
+        ${food.strMeasure6} ${food.strIngredient6}<br>
+        ${food.strMeasure7} ${food.strIngredient7}<br>
+        ${food.strMeasure8} ${food.strIngredient8}<br>
+        ${food.strMeasure9} ${food.strIngredient9}
+        `
+    }
